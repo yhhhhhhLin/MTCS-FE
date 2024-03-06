b033debcbc09fff03b64f1787768c1616f4d894e
lin <1583427850@qq.com>
2024-03-06 14:01:21 +0800

    feat: 积分商城构建

--- src/views/credit/Square.vue
+++ src/views/credit/Square.vue
@@ -1,37 +1,51 @@
 <template>
   <Container :navbarDefault="'/credit/square'">
     <template v-slot:content>
-      <a-card>
-        <p>
-          购买积分
-        </p>
+
+      <a-card title="购买积分">
         <div class="credit-products-square">
-          <a-card v-for="i in 10" :key="i" class="credit-product-item">
-            <div class="credit-item-body">
-              <div class="credit-item-body-title">
-                测试商品 {{i}}
+          <a-radio-group v-model="checkCredit">
+            <template v-for="item in 10" :key="item" class="credit-product-items">
+              <a-radio :value="item">
+                <template #radio="{ checked }">
+                  <a-space
+                      align="start"
+                      class="custom-radio-card"
+                      :class="{ 'custom-radio-card-checked': checked }"
+                  >
+                    <div class="custom-radio-card-mask">
+                      <div class="custom-radio-card-mask-dot"/>
+                    </div>
+                    <div>
+                      <div class="credit-product-item-title">
+                        <div class="credit-product-item-title-text">
+                          这是商品 {{ item }}
+                        </div>
+                        <div class="credit-product-item-title-price">
+                          19.{{item}}
+                        </div>
+                      </div>
+                      <a-typography-text type="secondary">
+                        <div class="credit-product-item-body">
+                          <div class="credit-product-item-body-img">
+                            <img src="../../assets/credit-product-img.png" style="width: 100%" height="200px" alt="这是图">
+                          </div>
 
-              </div>
-              <div class="credit-item-body-disc">
+                        </div>
 
-              </div>
-              <div class="credit-item-body-price">
+                      </a-typography-text>
+                    </div>
+                  </a-space>
+                </template>
+              </a-radio>
+            </template>
+          </a-radio-group>
 
-              </div>
-              <div class="credit-item-body-picture">
-
-              </div>
-              <div class="credit-item-body-checkbox">
-                <a-checkbox v-model="checkCredit"></a-checkbox>
-              </div>
-
-
-
-            </div>
-          </a-card>
+          <a-button @click="showCheckCreditId" type="primary">获取当前id</a-button>
         </div>
-      </a-card>
 
+
+      </a-card>
 
 
     </template>
@@ -46,22 +60,30 @@
 import Container from "../../components/Container.vue";
 import {ref} from "vue";
 
-const checkCredit = ref(false);
+const checkCredit = ref(0);
+
+
+function showCheckCreditId() {
+  alert(checkCredit.value)
+}
+
 </script>
 
 <style scoped>
 
-.credit-products-square{
+.credit-products-square {
   display: flex;
   flex-wrap: wrap;
-    .credit-product-item{
-      flex-basis: 20%;
-      margin: 5px;
-      height: 200px;
-      transition:  transform 0.3s;
+
+  .credit-product-item {
+    flex-basis: 20%;
+    margin: 5px;
+    height: 200px;
+    transition: transform 0.3s;
   }
 
 }
+
 .credit-product-item:hover {
   transform: scale(1.03); /* 当鼠标悬停在弹性项上时，将弹性项的大小缩放为原来的 1.1 倍 */
 }
@@ -72,6 +94,66 @@ const checkCredit = ref(false);
   flex-direction: column;
 
 
+}
+
+
+.custom-radio-card {
+  padding: 10px 16px;
+  margin-bottom: 20px;
+  border: 1px solid var(--color-border-2);
+  border-radius: 4px;
+  width: 250px;
+  box-sizing: border-box;
+}
+
+.custom-radio-card-mask {
+  height: 14px;
+  width: 14px;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 100%;
+  border: 1px solid var(--color-border-2);
+  box-sizing: border-box;
+}
+
+.custom-radio-card-mask-dot {
+  width: 8px;
+  height: 8px;
+  border-radius: 100%;
+}
+
+.credit-product-item-title {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: var(--color-text-1);
+  margin-bottom: 8px;
+}
+.credit-product-item-title-text{
+  font-size: 17px;
+  font-weight: bold;
+
+}
+
+.custom-radio-card:hover,
+.custom-radio-card-checked,
+.custom-radio-card:hover .custom-radio-card-mask,
+.custom-radio-card-checked .custom-radio-card-mask {
+  border-color: rgb(var(--primary-6));
+}
+
+.custom-radio-card-checked {
+  background-color: var(--color-primary-light-1);
+}
+
+.custom-radio-card:hover .credit-product-item-title,
+.custom-radio-card-checked .credit-product-item-title {
+  color: rgb(var(--primary-6));
+}
+
+.custom-radio-card-checked .custom-radio-card-mask-dot {
+  background-color: rgb(var(--primary-6));
 }
 
 </style>
